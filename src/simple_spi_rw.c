5dd5390d14d4ca85b78e77c885692c649f73968a
mikayong <skerlan@qq.com>
2019-07-24 05:14:56 -0400

    init source

---
+++ src/simple_spi_rw.c
@@ -0,0 +1,43 @@
+#include "radio.h"
+int DEBUG_INFO = 0;
+
+int main (int argc, char *argv[]) {
+
+    int rst_pin = 0, spidev = 0;
+    int ret = 1;
+
+    if (argc < 3) 
+        return ret;
+
+    rst_pin = atoi(argv[1]);
+
+    spidev = atoi(argv[2]);
+
+    if (rst_pin < 0 || rst_pin > 40) 
+        return ret;
+
+    radiodev *loradev;
+    loradev = (radiodev *) malloc(sizeof(radiodev));
+    loradev->nss = 25;
+    loradev->rst = rst_pin;
+    loradev->dio[0] = 4;
+    loradev->dio[1] = 0;
+    loradev->dio[2] = 0;	
+    strcpy(loradev->desc, "LG02 detect");	
+
+    if (spidev)
+        loradev->spiport = lgw_spi_open(SPI_DEV_TX);
+    else
+        loradev->spiport = lgw_spi_open(SPI_DEV_RX);
+
+    if (get_radio_version(loradev)) {  
+        ret = 0;
+    }
+
+    close(loradev->spiport);
+
+    free(loradev);
+
+    return ret;
+
+}
